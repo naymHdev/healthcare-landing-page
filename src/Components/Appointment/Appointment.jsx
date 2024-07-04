4786554800a4c0f074b6feb58395cad80ea28a88
MacBook Pro <naym100m@gmail.com>
2024-07-04 11:27:54 +0600

    created: a appoinment card section design with full responsive

--- src/Components/Appointment/Appointment.jsx
+++ src/Components/Appointment/Appointment.jsx
@@ -1,18 +1,42 @@
 import React from "react";
+import bgImage from "../../../public/Rectangle 32.png";
+import Button from "../Button/Button";
+import logo from "../../../public/logo light (1).png";
 
 const Appointment = () => {
   return (
-    <>
-      <div className="p-4 md:p-8 mx-auto max-w-7xl">
-        <div className="bg-appointment h-[70vh] bg-cover bg-no-repeat object-center rounded-[32px]">
-          <div className=" bg-black  w-full h-[70vh] rounded-[32px] bg-opacity-35">
-            <h2 className=" text-3xl font-semibold leading-10">
-              Get Your First Appointment at 50% Off!
-            </h2>
+    <div className="p-4 md:p-8 mx-auto max-w-7xl">
+      <div
+        className="relative h-[70vh] rounded-[32px] bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 rounded-[32px] md:flex justify-between text-white">
+          <div className=" md:order-last p-10">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="bg-gradient-to-r from-[#070546] via-[#1D1B56] via-30% rounded-[32px]">
+            <div className="mt-8 md:mt-[80px] p-10">
+              <div className="text-3xl md:text-5xl font-semibold space-y-2">
+                <p>Get Your</p>
+                <p>First Appointment</p>
+                <p>at 50% Off!</p>
+              </div>
+              <div className="md:flex gap-4 mt-5 md:mt-8 space-y-3 md:space-y-0">
+                <Button
+                  text="Appointment"
+                  backgroundColor="bg-yellow-500 hover:bg-yellow-600"
+                />
+                <Button
+                  text="Learn More"
+                  backgroundColor="hover:bg-white hover:text-black transition"
+                  border="border"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
